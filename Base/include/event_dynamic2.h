349d4a339c37f74a177aadf5a6a8655ef457656e
ottogithub <x20121223@163.com>
2017-01-16 11:05:22 +0800

    think about decay

--- Base/include/event_dynamic2.h
+++ Base/include/event_dynamic2.h
@@ -18,8 +18,8 @@ namespace TerraX
         struct wrapped : base {
             typedef std::function<void(Args...)> func_type;
             func_type f;
-            std::tuple<Args...> t;
-
+            //std::tuple<typename std::decay<Args>::type...> t;
+			std::tuple<Args...> t;
             wrapped(func_type&& aFunc) : f(aFunc){};
             std::size_t args_hashcode() override final { return typeid(t).hash_code(); }
             std::size_t args_size() override final { return std::tuple_size<decltype(t)>::value; }
@@ -40,18 +40,30 @@ namespace TerraX
         };
         // Call the function by casting the base
         //  pointer back to its derived type:
-        template <typename... Args>
-        void operator()(Args... args) const
-        {
-            // boost::polymorphic_downcast
-            // arguments are not same becauseof (typeid(pkt*) != typeid(message*));
-            // assert(p_base->args_hashcode() == typeid(std::tuple<Args...>).hash_code());
-
-            wrapped<Args...>* p_wrapped = static_cast<wrapped<Args...>*>(p_base.get());
-            if (p_wrapped) {
-                p_wrapped->f(args...);
-            }
-        };
+   //     template <typename... Args>
+   //     void operator()(Args&&... args) const
+   //     {
+   //         // boost::polymorphic_downcast
+   //         // arguments are not same becauseof (typeid(pkt*) != typeid(message*));
+   //         // assert(p_base->args_hashcode() == typeid(std::tuple<Args...>).hash_code());
+			////typename decay<Args>::type...
+   //         wrapped<Args...>* p_wrapped = static_cast<wrapped<Args...>*>(p_base.get());
+   //         if (p_wrapped) {
+   //             p_wrapped->f(std::forward<Args>(args)...);
+   //         }
+   //     };
+		template <typename... Args>
+		void operator()(Args... args) const
+		{
+			// boost::polymorphic_downcast
+			// arguments are not same becauseof (typeid(pkt*) != typeid(message*));
+			// assert(p_base->args_hashcode() == typeid(std::tuple<Args...>).hash_code());
+			//typename decay<Args>::type...
+			wrapped<Args...>* p_wrapped = static_cast<wrapped<Args...>*>(p_base.get());
+			if (p_wrapped) {
+				p_wrapped->f(args...);
+			}
+		};
 
         std::size_t args_size() { return p_base->args_size(); }
     };
